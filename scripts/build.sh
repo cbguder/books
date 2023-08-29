93640d4d0224dd2ffb6a4a8fa6f71a885201c758
Can Berk Güder <cbg@cbg.me>
2023-08-29 11:08:25 -0700

    Release action
    Create a draft release when a version tag is pushed. Also rename Go
    workflow to Test.

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+TAG=$(git describe --tags)
+
+mkdir -p release
+pushd release
+
+build() {
+  CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build ..
+  tar -czf "books-${TAG}-$1-$2.tar.gz" books
+  rm -f books
+}
+
+build darwin amd64
+build darwin arm64
+build linux amd64
+build linux arm64
+
+popd
